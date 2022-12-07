eaca26f641a61a8ba9c369b69571e8c523d051eb
JGarcia128 <jeidigarcia123@gmail.com>
2022-12-06 23:15:51 -0500

    working on adding dist folder

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -2,10 +2,34 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    console.log('hit')
+    console.log("event" + event)
+    event.preventDefault();
+    // Store the triggered events
+    window.deferredPrompt = event;
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+    // Remove the hidden class from the button.
+    butInstall.classList.toggle('hidden', false);
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+// Implementing an avent handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+        return;
+    }
+
+    promptEvent.prompt();
+
+    window.deferredPrompt = null;
+
+    butInstall.classList.toggle('hidden', true);
+});
+
+// Adding an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+        // Clear prompt
+        console.log('install hit')
+        window.deferredPrompt = null;
+});
